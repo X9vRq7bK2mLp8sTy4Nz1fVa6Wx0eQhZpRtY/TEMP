374da7e0b6dec0fbe33a80e24d9605999bb12b7f
AMIXNSUJNJNSIXNSIHXNSUHBXEUHBUHNKSJN <jawshcx@icloud.com>
2025-09-06 12:19:10 +0200

    Update text.js

--- api/text.js
+++ api/text.js
@@ -3,13 +3,15 @@ import { tokens } from "./get-token";
 export default function handler(req, res) {
   const token = req.headers["x-temp-token"];
   if (!token || !tokens[token] || tokens[token] < Date.now()) {
-    res.status(403).send("invalid or expired token 🚫");
+    res.status(403).send('print("invalid or expired token 🚫")'); // lua-friendly
     return;
   }
 
-  // optional: make token single-use
+  // single-use token
   delete tokens[token];
 
   res.setHeader("content-type", "text/plain");
-  res.send("hello roblox! this is raw text.");
+
+  // send lua-ready code as a string
+  res.send('print("hello roblox! this is raw lua code")');
 }
